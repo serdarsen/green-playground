48a6c308ff2b8ed9825a68700ede8c5316ce1a5c
serdarsen <s.sen@hubtic.com>
2022-11-06 07:54:14 +0300

    Add antd tailwindcss postcss autoprefixer. Add an example form. Add Readme.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Green Playground
+
+## Introduction
+
+- Green Playground is a project to play around with and test UI elements.
+
+## Tooling
+
+- [React](https://reactjs.org): An open source JavaScript library for building UIs
+  - [react](https://github.com/facebook/react): Core React library
+  - [@types/react](https://www.npmjs.com/package/@types/react): Type definitions for `React`
+  - [react-dom](https://www.npmjs.com/package/react-dom): Renders React to DOM
+  - [@types/react-dom](https://www.npmjs.com/package/@types/react-dom): Type definitions for `ReactDOM`
+- [TypeScript](https://www.typescriptlang.org/): A strongly typed language built on top of JavaScript.
+  - [typescript](https://github.com/Microsoft/TypeScript): Core TypeScript programming language
+- [Vite](https://vitejs.dev/): Next Generation Frontend Tooling.
+  - [@vitejs/plugin-react](https://github.com/vitejs/vite/tree/main/packages/plugin-react#readme): The all-in-one Vite plugin for React projects.
+- [Ant Design](https://ant.design/): An enterprise-class UI design language and React UI library..
+- [Tailwind CSS](https://tailwindcss.com/): A utility-first CSS framework for rapidly building custom user interfaces.
+- [Postcss](https://github.com/postcss/postcss): PostCSS is a tool for transforming styles with JS plugins.
+  - [Autoprofixer](https://github.com/postcss/autoprefixer): [PostCSS](https://github.com/postcss/postcss) plugin to parse CSS and add vendor prefixes to CSS rules.
+
+## Usage
+
+- The project built with `node v19.0.0`, `yarn 1.22.17`, and `npx 8.19.2`.
+- To be able to develop locally, run `yarn run dev` and browse `localhost:5173`
+
+## Script
+
+| Command        | Description                                                       |
+| -------------- | ----------------------------------------------------------------- |
+| `yarn install` | Install all dependent application packages                        |
+| `yarn run dev` | Start a local development web server at localhost:5173 using vite |
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,99 @@
+import {
+	Button,
+	Cascader,
+	DatePicker,
+	Form,
+	Input,
+	InputNumber,
+	Radio,
+	Select,
+	Switch,
+	TreeSelect,
+} from 'antd';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 
-function App() {
-	const [count, setCount] = useState(0);
+type SizeType = Parameters<typeof Form>[0]['size'];
+
+const App: React.FC = () => {
+	const [componentSize, setComponentSize] = useState<SizeType | 'default'>(
+		'default'
+	);
+
+	const onFormLayoutChange = ({ size }: { size: SizeType }) => {
+		setComponentSize(size);
+	};
 
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React Hello</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+		<div className="h-screen w-screen flex flex-row justify-center items-center">
+			<Form
+				labelCol={{ span: 4 }}
+				wrapperCol={{ span: 14 }}
+				layout="horizontal"
+				initialValues={{ size: componentSize }}
+				onValuesChange={onFormLayoutChange}
+				size={componentSize as SizeType}
+				className="w-4/12"
+			>
+				<Form.Item label="Form Size" name="size">
+					<Radio.Group>
+						<Radio.Button value="small">Small</Radio.Button>
+						<Radio.Button value="default">Default</Radio.Button>
+						<Radio.Button value="large">Large</Radio.Button>
+					</Radio.Group>
+				</Form.Item>
+				<Form.Item label="Input">
+					<Input />
+				</Form.Item>
+				<Form.Item label="Select">
+					<Select>
+						<Select.Option value="demo">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore
+						</Select.Option>
+					</Select>
+				</Form.Item>
+				<Form.Item label="TreeSelect">
+					<TreeSelect
+						treeData={[
+							{
+								title: 'Light',
+								value: 'light',
+								children: [{ title: 'Bamboo', value: 'bamboo' }],
+							},
+						]}
+					/>
+				</Form.Item>
+				<Form.Item label="Cascader">
+					<Cascader
+						options={[
+							{
+								value: 'zhejiang',
+								label: 'Zhejiang',
+								children: [
+									{
+										value: 'hangzhou',
+										label: 'Hangzhou',
+									},
+								],
+							},
+						]}
+					/>
+				</Form.Item>
+				<Form.Item label="DatePicker">
+					<DatePicker />
+				</Form.Item>
+				<Form.Item label="InputNumber">
+					<InputNumber />
+				</Form.Item>
+				<Form.Item label="Switch" valuePropName="checked">
+					<Switch />
+				</Form.Item>
+				<Form.Item label="Button">
+					<Button>Button</Button>
+				</Form.Item>
+			</Form>
 		</div>
 	);
-}
+};
 
 export default App;
